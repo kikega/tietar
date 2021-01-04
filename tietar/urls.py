aa9126a8531f40875b38bcfd77ad2b65c297eb55
Enrique Galvez <kikega@telefonica.net>
2021-01-04 21:32:17 +0100

    Arreglo de index

--- tietar/urls.py
+++ tietar/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from landing import views as landing_views
+
 urlpatterns = [
+    path('', landing_views.index_view, name='index'),
     path('admin/', admin.site.urls),
 ]
